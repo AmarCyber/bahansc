88d14788008bf339628273c5d96b1a3cb0d63b3d
bintang2019 <47324989+bintang2019@users.noreply.github.com>
2019-05-18 17:02:17 +0700

    Add files via upload

--- bahan.py
+++ bahan.py
@@ -33,32 +33,32 @@
                         pass
                                                     
 
-        if op.type == 13: 
-            if op.param3 in wait["blacklist"]: 
+        if op.type == 13:
+            if op.param3 in wait["blacklist"]:
                 if op.param2 not in Bots and op.param2 not in owner and op.param2 not in admin and op.param2 not in staff:
                     try:
-                        group = random.choice(ABC).getGroup(op.param1)
+                        group = cl.getGroup(op.param1)
                         gMembMids = [contact.mid for contact in group.invitee]
                         for _mid in gMembMids:
-                        	if _mid not in Bots:
+                            if _mid not in Bots:
                                 random.choice(ABC).cancelGroupInvitation(op.param1,[_mid])
                                 random.choice(ABC).kickoutFromGroup(op.param1,[op.param2])
-                        except:
-                            pass
+                    except:
+                        pass
                                                     
 
-        if op.type == 13: 
-            if op.param2 in wait["blacklist"]: 
+        if op.type == 13:
+            if op.param2 in wait["blacklist"]:
                 if op.param2 not in Bots and op.param2 not in owner and op.param2 not in admin and op.param2 not in staff:
                     try:
-                        group = random.choice(ABC).getGroup(op.param1)
+                        group = cl.getGroup(op.param1)
                         gMembMids = [contact.mid for contact in group.invitee]
                         for _mid in gMembMids:
-                        	if _mid not in Bots:
+                            if _mid not in Bots:
                                 random.choice(ABC).cancelGroupInvitation(op.param1,[_mid])
                                 random.choice(ABC).kickoutFromGroup(op.param1,[op.param2])
-                        except:
-                            pass
+                    except:
+                        pass
                         
         if op.type == 19:
             if mid in op.param3:
